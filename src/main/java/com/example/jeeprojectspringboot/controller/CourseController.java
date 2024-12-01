20543827250e507ab69a7760f2f203f0f6f14139
JTeam1 <julienguyot42@gmail.com>
2024-12-01 19:37:45 +0100

    fix : add one professor for one classe or promo or pathway for one subject

--- src/main/java/com/example/jeeprojectspringboot/controller/CourseController.java
+++ src/main/java/com/example/jeeprojectspringboot/controller/CourseController.java
@@ -30,15 +30,18 @@ public class CourseController {
     @GetMapping("/courses")
     public String login(HttpSession session, Model model) {
         if (session.getAttribute("user") != null && Admin.class.getName().equals(session.getAttribute("role"))) {
-            model.addAttribute("courses", courseService.getAllCourses());
-            model.addAttribute("subjects", subjectService.getAllSubjects()); // Ajoutez les matières
-            model.addAttribute("professors", professorService.getAllProfessors()); // Ajoutez les professeurs
-            model.addAttribute("groups", studentGroupService.getAllStudentGroups()); // Ajouter les groupes
+            setModelAttributes(model);
             return "course";
         }
         return "login";
     }
 
+    private void setModelAttributes(Model model){
+        model.addAttribute("courses", courseService.getAllCourses());
+        model.addAttribute("subjects", subjectService.getAllSubjects()); // Ajoutez les matières
+        model.addAttribute("professors", professorService.getAllProfessors()); // Ajoutez les professeurs
+        model.addAttribute("groups", studentGroupService.getAllStudentGroups()); // Ajouter les groupes
+    }
 
     @PostMapping("/courses")
     public String saveCourse(@RequestParam("subjectId") Long subjectId,
@@ -57,6 +60,7 @@ public class CourseController {
                 Professor professor = professorService.findProfessorById(professorId);
                 if (subject == null || professor == null) {
                     model.addAttribute("errorMessage","Sujet ou professeur non valide");
+                    setModelAttributes(model);
                     return "course";
                 }
                 Course course;
@@ -64,6 +68,7 @@ public class CourseController {
                     Course existingCourse = courseService.getSelectedCourse(id);
                     if (existingCourse == null) {
                         model.addAttribute("errorMessage","cours non valide");
+                        setModelAttributes(model);
                         return "course";
                     }
                     course = existingCourse;
@@ -72,6 +77,7 @@ public class CourseController {
                 }
                 if (!professor.getTeachingSubjects().contains(subject)) {
                     model.addAttribute("errorMessage", "Ce professeur ne peut pas enseigner cette matière");
+                    setModelAttributes(model);
                     return "course";
                 }
                 List<StudentGroup> studentGroups = new ArrayList<>();
@@ -80,10 +86,48 @@ public class CourseController {
                     studentGroup = studentGroupService.getStudentGroupFromId(groupId);
                     if (studentGroup == null) {
                         model.addAttribute("errorMessage", "Groupes non valides");
+                        setModelAttributes(model);
                         return "course";
+                    }
+                    List<Course> coursesBySubject = courseService.getCoursesBySubject(subject);
+                    for(Course course1 : coursesBySubject){
+                        if(course1.getSubject().equals(subject)){
+                            if(course.getId() == null) {
+                                if (course1.getStudentGroups().contains(studentGroup)) {
+                                    model.addAttribute("errorMessage", "Au moins un des groupes sélectionnés a déja ce cours avec un autre professeur");
+                                    setModelAttributes(model);
+                                    return "course";
+                                } else if (course1.getProfessor().equals(professor)) {
+                                    model.addAttribute("errorMessage", "Au moins un des groupes sélectionnés a déja ce cours avec ce professeur");
+                                    setModelAttributes(model);
+                                    return "course";
+                                } else if(studentGroup instanceof Classe){
+                                    Classe classe = (Classe) studentGroup;
+                                    if(course1.getStudentGroups().contains(classe.getPromo()) || course1.getStudentGroups().contains(classe.getPathway())){
+                                        model.addAttribute("errorMessage", "Au moins un des groupes sélectionnés a déja ce cours avec ce professeur qui enseigne à toute une filière ou une promo");
+                                        setModelAttributes(model);
+                                        return "course";
+                                    }
+                                }
+                            } else {
+                                if (course1.getStudentGroups().contains(studentGroup) && course.getProfessor().equals(professor)) {
+                                    model.addAttribute("errorMessage", "Au moins un des groupes sélectionnés a déja ce cours avec un autre professeur");
+                                    setModelAttributes(model);
+                                    return "course";
+                                } else if(studentGroup instanceof Classe){
+                                    Classe classe = (Classe) studentGroup;
+                                    if(course1.getStudentGroups().contains(classe.getPromo()) || course1.getStudentGroups().contains(classe.getPathway())){
+                                        model.addAttribute("errorMessage", "Au moins un des groupes sélectionnés a déja ce cours avec ce professeur qui enseigne à toute une filière ou une promo");
+                                        setModelAttributes(model);
+                                        return "course";
+                                    }
+                                }
+                            }
+                        }
                     }
                     studentGroups.add(studentGroup);
                 }
+
                 course.setStudentGroups(studentGroups);
                 course.setSubject(subject);
                 course.setProfessor(professor);
@@ -100,5 +144,20 @@ public class CourseController {
         } else {
             return "error";
         }
+    }
+
+    private boolean isSamePromoOrPathway(Course course1, StudentGroup studentGroup, Professor professor) {
+        if (studentGroup instanceof Promo) {
+            Promo promo = (Promo) studentGroup;
+            if (course1.getStudentGroups().contains(promo) && course1.getProfessor().equals(professor)) {
+                return true;
+            }
+        } else if (studentGroup instanceof Pathway) {
+            Pathway pathway = (Pathway) studentGroup;
+            if (course1.getStudentGroups().contains(pathway) && course1.getProfessor().equals(professor)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
